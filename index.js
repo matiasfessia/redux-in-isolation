4ba3c543cc0fa652f80e773dc3f958088874740e
Matias Fessia <matiasfessia@gmail.com>
2019-11-22 11:45:12 -0300

    creating a store and dispatching actions

---
+++ index.js
@@ -0,0 +1,45 @@
+const { createStore } = require('redux');
+
+const store = createStore((state = { count: 0 }, action) => {
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + 1
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - 1
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
+});
+
+console.log(store.getState());
+
+// Actions - than an object that gets sents to the store
+
+// I'd like to increment the count
+store.dispatch({
+  type: 'INCREMENT'
+});
+store.dispatch({
+  type: 'INCREMENT'
+});
+console.log(store.getState());
+
+// I'd like to reset the count to zero
+store.dispatch({
+  type: 'RESET'
+});
+console.log(store.getState());
+
+// I'd like to decrement the count
+store.dispatch({
+  type: 'DECREMENT'
+});
+console.log(store.getState());
